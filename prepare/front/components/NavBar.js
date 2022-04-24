748a04812fcf28dc441fd39571f17c77fe0aae72
고윤혁 <sksgur3217@gmail.com>
2022-04-25 08:28:32 +0900

    nav bar css done

--- prepare/front/components/NavBar.js
+++ prepare/front/components/NavBar.js
@@ -50,6 +50,7 @@ const NavBar = () => {
           </div>
           <div id="icon-button">
             <h3 className="blind">아이콘 버튼</h3>
+            <h4 className="blind">홈</h4>
             <a>
               <svg
                 aria-label="홈"
@@ -68,6 +69,7 @@ const NavBar = () => {
                   stroke-width="2"></path>
               </svg>
             </a>
+            <h4 className="blind">다이렉트 메세지</h4>
             <a>
               <svg
                 aria-label="Direct"
@@ -83,6 +85,7 @@ const NavBar = () => {
                   fill-rule="evenodd"></path>
               </svg>
             </a>
+            <h4 className="blind">새로운 메세지</h4>
             <a>
               <svg
                 aria-label="새로운 게시물"
@@ -122,6 +125,7 @@ const NavBar = () => {
                   y2="17.455"></line>
               </svg>
             </a>
+            <h4 className="blind">사람 찾기</h4>
             <a>
               <svg
                 aria-label="사람 찾기"
@@ -151,6 +155,7 @@ const NavBar = () => {
                   stroke-width="2"></circle>
               </svg>
             </a>
+            <h4 className="blind">활동 피드</h4>
             <a>
               <svg
                 aria-label="활동 피드"
@@ -164,6 +169,7 @@ const NavBar = () => {
                 <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
               </svg>
             </a>
+            <h4 className="blind">User</h4>
             <Avatar src={<img className="user-img" src="https://joeschmoe.io/api/v1/random" />} />
           </div>
         </nav>

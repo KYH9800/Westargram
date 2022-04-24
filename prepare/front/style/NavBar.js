e6bcfaf734bb92244978712aef93565419e9fe57
고윤혁 <sksgur3217@gmail.com>
2022-04-25 08:20:13 +0900

    nav bar css done

--- prepare/front/style/NavBar.js
+++ prepare/front/style/NavBar.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   #navWrapper {
     margin: 0 auto;
-    max-width: 975px;
+    max-width: 990px;
   }
   #navWrapper > nav {
     padding: 0 20px;
@@ -15,13 +15,18 @@ export const Wrapper = styled.div`
   }
   #main-logo {
     margin-top: 7px;
+    margin-right: 20px;
+    flex: 0.65 0 auto;
+    vertical-align: middle;
   }
   #main-logo .logo-img {
-    max-height: 100px;
-    max-width: 100px;
+    max-height: 102px;
+    max-width: 102px;
+    margin-top: 4px;
   }
   #search-input {
-    position: relative;
+    /* position: relative;
+    right: 16px; */
     display: inline-flex;
     align-items: center;
     background-color: #efefef;
@@ -30,10 +35,25 @@ export const Wrapper = styled.div`
     border-radius: 7px;
     vertical-align: middle;
   }
-  #search-input > span {
-    /* position: absolute;
-    left: 10px; */
+
+  @media (max-width: 640px) {
+    #search-input {
+      display: none;
+    }
   }
+
+  #search-input > span {
+    color: #8e8e8e;
+    padding: 0 16px;
+  }
+
+  #search-input span svg {
+    width: 16px;
+    height: 16px;
+    position: absolute;
+    top: 21.7px;
+  }
+
   #search-input input {
     background-color: #efefef;
     border: none;
@@ -42,7 +62,25 @@ export const Wrapper = styled.div`
     outline: none;
     padding: 0 2%;
     vertical-align: middle;
+    font-weight: 100;
+    font-size: 16px;
+    padding-left: 12px;
   }
   #icon-button {
+    display: flex;
+    padding-left: 24px;
+    margin-top: 5px;
+    vertical-align: middle;
+  }
+
+  #icon-button svg {
+    margin: 0 12px;
+  }
+
+  #icon-button .user-img {
+    width: 24px;
+    margin-left: 10px;
+    border-radius: 50%;
+    background-color: gray;
   }
 `;
